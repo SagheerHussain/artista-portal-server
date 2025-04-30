fe8450a97f230376e13e896994f021d08b65e903
Sagheer Hussain <68946945+SagheerHussain@users.noreply.github.com>
2025-04-30 13:19:27 +0500

    bug fix

--- currencyExchangeRates.js
+++ currencyExchangeRates.js
@@ -1,12 +1,12 @@
 const getUSDToPKRExchangeRates = async () => {
-    try {
-        const response = await fetch("https://open.er-api.com/v6/latest/USD");
-        const data = await response.json();
-        return data.rates.PKR;
-    } catch (error) {
-        console.error("Error fetching USD to PKR exchange rate:", error);
-        return null;
-    }
-}
+  try {
+    const response = await fetch("https://open.er-api.com/v6/latest/USD");
+    const data = await response.json();
+    return data.rates.PKR;
+  } catch (error) {
+    console.error("Error fetching USD to PKR exchange rate:", error);
+    return null;
+  }
+};
 
-module.exports = getUSDToPKRExchangeRates;
+module.exports = { getUSDToPKRExchangeRates };
